a422425e047f4d4a95915c935ac09960936f8745
vedant joshi <30870673+Anukriti-joshi@users.noreply.github.com>
2026-02-28 01:57:38 +0530

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
+  "mess-management-system-backend-production.up.railway.app",
   "https://mess-management-frontend-xs92.onrender.com",
   "http://localhost:3000",
-  "http://127.0.0.1:3000"
+  "http://127.0.0.1:3000",
 ];
 
 export default allowedOrigins;
